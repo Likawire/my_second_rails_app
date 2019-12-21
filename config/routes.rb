75b923273be116d8dc58cbc9f0435057730791ae
Cephas Likawire <cmcephasx686@gmail.com>
2019-12-21 15:17:19 +0200

    updates for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :families
+  root 'family#index'
+  resources :family
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
